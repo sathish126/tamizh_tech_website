eb1ed422a8a5f8fac3f97abca0a92addf9293124
SATHISH KUMAR P <147464469+sathish126@users.noreply.github.com>
2025-08-03 19:06:27 +0530

    prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import ProductCatalog from './components/ProductCatalog';
 import About from './components/About';
 import Team from './components/Team';
 import Contact from './components/Contact';
@@ -19,7 +18,6 @@ function App() {
       <Hero />
       <NewArrivals />
       <Robokits />
-      <ProductCatalog />
       <About />
       <OurPartners />
       <Testimonial />
